f13e3e09b1e38068fad143741090aa565fb49aec
Anuja104 <120162811+Anuja104@users.noreply.github.com>
2022-12-09 12:06:13 +0530

    Add files via upload

---
+++ Final_Assignment.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>My Jupyter Notebook on IBM Watson Studio</h1>
+
+# <b> Anuja Kharatmol </b>
+
+# <i> I am interested in data science because I love to gain insights form data </i>
+
+# The following code calculate the factorial of number
+
+# In[2]:
+
+
+print("Enter Number:")
+number=int(input())
+number_entered=number
+fac=1
+if number==0:
+    result=1
+else:
+    while number>=1:
+        fac=fac*number
+        number=number-1
+    result=fac
+    
+print('Factorial of {} is {}'.format(number_entered,result))
+
+
+# <ul>
+#   <li>Python</li>
+#   <li>R</li>
+#   <li>SQL</li>
+#   <li>Algebra</li>
+# </ul>
+
+# <img src="Image1.jpg" alt="Flowers">
+
+# <table>
+#   <tr>
+#     <td>XYZ</td>
+#     <td>PQR</td>
+#     <td>ABC</td>
+#   </tr>
+#   <tr>
+#     <td>16</td>
+#     <td>14</td>
+#     <td>10</td>
+#   </tr>
+# </table>
+
+# <a href="https://www.ibm.com/">Visit ibm.com!</a>
+
+# In[ ]:
+
+
+
+
